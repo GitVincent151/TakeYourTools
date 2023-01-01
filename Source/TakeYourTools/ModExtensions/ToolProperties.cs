22fc360612c23b3305ed0eb8ee11cfd4001f05f1
GitVincent <gitvincent@mail.de>
2023-01-01 10:53:52 +0100

    Project start

---
+++ Source/TakeYourTools/ModExtensions/ToolProperties.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace TakeYourTools
+{
+    public class ToolProperties : DefModExtension
+    {
+
+        public static readonly ToolProperties defaultValues = new ToolProperties();
+
+        public List<StatModifier> baseWorkStatFactors;
+
+        //[NoTranslate]
+        public List<string> defaultToolAssignmentTags;
+
+    }
+}
